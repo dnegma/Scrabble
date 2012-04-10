7ac8856ab629d011c4f38ef9749781c239ef5f64
Diana Gren <dianagren@gmail.com>
2012-04-10 17:50:47 +0200

    Merge plots

--- results/Plots/src/BalanceWinsAgainstHighScore.m
+++ results/Plots/src/BalanceWinsAgainstHighScore.m
@@ -2,14 +2,14 @@
 x = [4, 0, 46, 20, 25, 38, 83, 12, 132]; 
 
 %HighScoreWordPlayer
-y = [9, 1000, 954, 979, 975, 960, 915, 988, 866];
+%y = [9, 1000, 954, 979, 975, 960, 915, 988, 866];
 
 z = [0:8];
 
-plot(z,x, 'g');
+plot(z,x);
 hold on
-plot(z,y);
-hold off
-legend('BalanceOnRackPlayer', 'HighScoreWordPlayer');
-xlabel('Game');
-ylabel('Score');
+%plot(z,y);
+%hold off
+legend('BalanceOnRackPlayer');
+xlabel('Vowels');
+ylabel('Wins');
